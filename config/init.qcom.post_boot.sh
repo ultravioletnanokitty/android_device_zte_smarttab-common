c783f15c7cb07202ff9cf576c2dd262e0faaf042
ultraviolet <ultravioletnanokitty@gmail.com>
2014-01-29 13:28:27 -0700

    switched cpus to SavagedZen governor by default, fixed adb root

--- config/init.qcom.post_boot.sh
+++ config/init.qcom.post_boot.sh
@@ -12,7 +12,7 @@ echo 1 > /sys/module/pm_8x60/modes/cpu0/power_collapse/idle_enabled
 echo 1 > /sys/module/pm_8x60/modes/cpu0/standalone_power_collapse/idle_enabled
 chown system /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq
 chown system /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq
-echo "smartassV2" > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
+echo "SavagedZen" > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
 echo 384000 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq
 # cpu1
 chown root.system /sys/devices/system/cpu/cpu1/online
@@ -23,7 +23,7 @@ echo 1 > /sys/module/pm_8x60/modes/cpu1/power_collapse/idle_enabled
 echo 1 > /sys/module/pm_8x60/modes/cpu1/standalone_power_collapse/idle_enabled
 chown system /sys/devices/system/cpu/cpu1/cpufreq/scaling_max_freq
 chown system /sys/devices/system/cpu/cpu1/cpufreq/scaling_min_freq
-echo "smartassV2" > /sys/devices/system/cpu/cpu1/cpufreq/scaling_governor
+echo "SavagedZen" > /sys/devices/system/cpu/cpu1/cpufreq/scaling_governor
 echo 384000 > /sys/devices/system/cpu/cpu1/cpufreq/scaling_min_freq
 # mfreq
 chown root.system /sys/devices/system/cpu/mfreq
